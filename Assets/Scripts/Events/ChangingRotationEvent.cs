b06c2273bea35a51cc57197abd08b57fbacbc11c
unknown <jjc6252498@gmail.com>
2025-04-03 16:46:35 +0900

    [Add] 오브젝트 변환, 오브젝트 회전 이상현상 구현

---
+++ Assets/Scripts/Events/ChangingRotationEvent.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ChangingRotationEvent : GameEvent
+{
+    [SerializeField] private Transform _myTransform;
+    [SerializeField] private Vector3 _originEulerVec = Vector3.zero;
+    [SerializeField] private Vector3 _targetEulderVec = Vector3.zero;
+
+    protected override void Execute()
+    {
+        _myTransform.rotation = Quaternion.Euler(_targetEulderVec);
+    }
+
+    protected override void ResetEvent()
+    {
+        _myTransform.rotation = Quaternion.Euler(_originEulerVec);
+    }
+}
